18e031e7e43f7e93663ef6f79cdc2d91a9ea9b7e
Eduarda Lima <eududalim@gmail.com>
2020-04-15 10:39:37 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink[100],
+      child: Center(
+        child: Text("Safe App", 
+          style: TextStyle(color: Colors.pink[600], fontSize: 14.0)
+        )
+      ),
+    );
+  }
+}
